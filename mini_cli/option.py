7d863a8365f51a82a564b94855e4c49714acacd4
RichtersFinger <steffen.finger.privat@gmail.com>
2025-07-12 00:08:33 +0200

    [option] fix Option-constructor and -parsing

--- mini_cli/option.py
+++ mini_cli/option.py
@@ -1,6 +1,7 @@
 """Definitions for class `Option`."""
 
 from typing import Iterable, Optional, Callable, Any
+import sys
 
 
 class Option:
@@ -12,13 +13,21 @@ class Option:
         *,
         helptext: Optional[str] = None,
         nargs: Optional[int] = 1,
+        strict: bool = True,
         parser: Optional[
             Callable[[str], tuple[bool, Optional[str], Optional[Any]]]
         ] = None,
     ) -> None:
+        if len(names) == 0:
+            raise ValueError("An Option requires at least one name.")
+        if nargs < 0:
+            raise ValueError(
+                "Options do not support negative values for 'nargs'."
+            )
         self.__names = names
         self.__helptext = helptext
         self.__nargs = nargs
+        self.__strict = strict
         self.__parser = parser
 
     @property
@@ -36,8 +45,16 @@ class Option:
         """Returns `Option` nargs."""
         return self.__nargs
 
+    @property
+    def strict(self) -> bool:
+        """Returns `Option` strict."""
+        return self.__strict
+
     def parse(self, data: Any) -> tuple[bool, Optional[str], Any]:
         """Returns response of `Option`'s parser if available."""
         if self.__parser:
-            return self.__parser(data)
+            ok, msg, data = self.__parser(data)
+            if not ok:
+                print(msg, file=sys.stderr)
+                sys.exit(1)
         return True, None, data
